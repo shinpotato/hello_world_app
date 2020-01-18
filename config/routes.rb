1f3f68ea3b3eac27a225caede54255cf7239e53d
shinpotato <potatoandtomato2@gmail.com>
2020-01-18 16:03:39 +0900

    crud_wo_resources

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,16 @@ post '/users' => 'users#create'
 #一覧画面
 get '/users' => 'users#index'
 
+#動作確認
 get '/users/test' => 'users#test'
+
+#削除実行
+delete '/users/:id' => 'users#destroy'
+
+#編集画面へ
+get '/users/:id/edit' => 'users#edit'
+
+#編集実行
+patch 'users/:id' => 'users#update'
 
 end
